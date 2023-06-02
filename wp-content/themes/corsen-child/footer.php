7bf512f1f77f34cf2452be28f478e5b664ce9217
Bigfoot <felix@trademarkers.com>
2023-06-02 14:40:32 +0800

    updates

--- wp-content/themes/corsen-child/footer.php
+++ wp-content/themes/corsen-child/footer.php
@@ -13,21 +13,9 @@
 	do_action( 'corsen_action_before_body_tag_close' );
 	?>
 <?php wp_footer(); ?>
-<div class="popUpNotification closeNtification" style="position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background: rgba(0,0,0,0.4);
-    z-index: 199;
-	display:none">
-	<div class="message-content" style="    max-width: 500px;
-    min-height: 300px;
-    background: #fdfdfd;
-    margin: auto;
-    position: relative;
-    top: 35%;">
-		<div class="closeNtification">x</div>
+<div class="popUpNotification closeNtification">
+	<div class="message-content">
+		<div class="closeNtification btn-close">x</div>
 		<div class="message"></div>
 	</div>
 </div>
