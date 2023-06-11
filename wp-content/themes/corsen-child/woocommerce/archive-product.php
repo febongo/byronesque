9362195c96815c3e036f40027acd8a9c7fd82d88
Bigfoot <felix@trademarkers.com>
2023-06-11 14:19:24 +0800

    update

--- wp-content/themes/corsen-child/woocommerce/archive-product.php
+++ wp-content/themes/corsen-child/woocommerce/archive-product.php
@@ -138,7 +138,7 @@ if ( woocommerce_product_loop() ) {
  */
 do_action( 'woocommerce_after_main_content' );
 ?>
-<section class="elementor-section elementor-top-section elementor-element elementor-element-6a5b7b3 elementor-section-full_width elementor-section-stretched elementor-section-content-middle elementor-section-height-default elementor-section-height-default" data-id="6a5b7b3" data-element_type="section" data-settings="{&quot;stretch_section&quot;:&quot;section-stretched&quot;}" style="width: 1585px; left: 0px;">
+<section class="contact-marquee-container" data-id="6a5b7b3" data-element_type="section" data-settings="{&quot;stretch_section&quot;:&quot;section-stretched&quot;}" style="width: 1585px; left: 0px;">
 						<div class="elementor-container elementor-column-gap-no">
 					<div class="make-column-clickable-elementor elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-5b88c81" style="cursor: pointer;" data-column-clickable="https://byronesque.comcontact/" data-column-clickable-blank="_self" data-id="5b88c81" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
 			<div class="elementor-widget-wrap elementor-element-populated">
