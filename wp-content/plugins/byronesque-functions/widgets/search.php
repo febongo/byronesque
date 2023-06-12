94c66f41891054c3a6aafddb681fc5172ae1f723
Bigfoot <felix@trademarkers.com>
2023-06-12 21:29:01 +0800

    updates

--- wp-content/plugins/byronesque-functions/widgets/search.php
+++ wp-content/plugins/byronesque-functions/widgets/search.php
@@ -109,36 +109,36 @@ function search_product() {
         /**
          * NEW APPROACH TO IMPROVE SEARCH RESULTS
          */
-        $searchArrayKeyword = explode($keyword," ");
-        $searchArrayKeyword[] = $keyword;
+        $searchArrayKeyword = explode(" ", $keyword);
+        if (count($searchArrayKeyword) > 1) {
+            $searchArrayKeyword[] = $keyword;
+            // $keyword = $searchArrayKeyword[0];
+        }
+        
+        // var_dump($searchArrayKeyword);
         $args = array(
             'post_type'      => 'product',
             'post_status'    => 'publish',
             'posts_per_page' => -1,
-            's'              => 'your-search-keyword',
-            'tax_query' => array(
+            's'              => $keyword, // Replace with your desired search keyword
+            'tax_query'      => array(
                 'relation' => 'OR',
                 array(
-                    'taxonomy' => 'product_cat',
-                    'field' => 'name',
-                    'terms' => $searchArrayKeyword, // Replace with your desired category slugs
+                    'taxonomy' => 'product_cat', // Replace with the desired taxonomy (e.g., 'product_tag' for tags)
+                    'field'    => 'name',
+                    'terms'    => $searchArrayKeyword, // Replace with the desired category term
                 ),
                 array(
-                    'taxonomy' => 'product_designer',
-                    'field' => 'name',
-                    'terms' => $searchArrayKeyword, // Replace with your desired category slugs
-                ),
-                array(
-                    'taxonomy' => 'product_tag',
-                    'field' => 'name',
-                    'terms' => $searchArrayKeyword, // Replace with your desired category slugs
+                    'taxonomy' => 'product_designer', // Replace with your custom taxonomy slug or name
+                    'field'    => 'slug',
+                    'terms'    => $searchArrayKeyword, // Replace with the desired custom term
                 ),
             ),
         );
         
         $query = new WC_Product_Query( $args );
         $query_results = $query->get_products();
-
+        // var_dump($query_results);
         // SEARCH DESIGNERS
         $designers = termSearch($keyword,'product_designer');
 
@@ -160,6 +160,7 @@ function search_product() {
             's' => $keyword,
             'category' => 89,
             'numberposts'      => 2,
+            'post_status'      => 'publish',
             'orderby'          => 'date',
             'order'            => 'DESC',
             // Rest of your arguments
@@ -199,29 +200,29 @@ function search_product() {
 
             $output = '';
 
-            foreach ($query_results as $result) {
-
-                $price      = get_post_meta($result->ID,'_regular_price');
-                $price_sale = get_post_meta($result->ID,'_sale_price');
+            foreach ($query_results as $product) {
+                $price      = get_post_meta($product->get_id(),'_regular_price');
+                $price_sale = get_post_meta($product->get_id(),'_sale_price');
                 $currency   = get_woocommerce_currency_symbol();
-                $product = new WC_Product($result->ID);
-
-                $pexcerpt = strip_tags($result->post_content);
-                if (strlen($pexcerpt) > 100) {
-                    $pexcerpt = substr($pexcerpt, 0, 100);
-                    $pexcerpt = substr($pexcerpt, 0, strrpos($excerpt, ' '));
-                    $pexcerpt .= '...';
-                }
-                $subHeaderTitle = $product->short_description;
+                // $product = new WC_Product($result->ID);
+                
+                // var_dump($product->get_id());
+                // $pexcerpt = strip_tags($result->post_content);
+                // if (strlen($pexcerpt) > 100) {
+                //     $pexcerpt = substr($pexcerpt, 0, 100);
+                //     $pexcerpt = substr($pexcerpt, 0, strrpos($excerpt, ' '));
+                //     $pexcerpt .= '...';
+                // }
+                $subHeaderTitle = $product->get_short_description();
                 // echo "short >> $product->short_description";
                 $output .= '<li>';
-                    $output .= '<a href="'.get_post_permalink($result->ID).'">';
+                    $output .= '<a href="'.get_post_permalink($product->get_id()).'">';
                         $output .= '<div class="product-image">';
-                            $output .= '<img src="'.esc_url(get_the_post_thumbnail_url($result->ID,'full')).'">';
+                            $output .= '<img src="'.esc_url(get_the_post_thumbnail_url($product->get_id(),'full')).'">';
                         $output .= '</div>';
                         $output .= '<div class="product-data">';
-                            $output .= '<h6>'.$result->post_title.'</h6>';
-                            $output .= '<p>'.($subHeaderTitle ? $subHeaderTitle : $pexcerpt).'</p>';
+                            $output .= '<h6>'.$product->get_title().'</h6>';
+                            $output .= '<p>'.$subHeaderTitle.'</p>';
                             if (!empty($price)) {
                                 $output .= '<div class="product-price">';
                                     $output .= $product->get_price_html();
@@ -234,15 +235,17 @@ function search_product() {
                 $output .= '</li>';
             }
 
-            if (!empty($output)) {
-                $shopUrl = get_permalink( wc_get_page_id( 'shop' ) );
-                echo $designerStr;
-                echo "<div class='product-search searchedBlock'><h3>Products</h3><ul>";
-                echo $output;
-                echo "</ul><a href='$shopUrl' class='btn-search'>VIEW ALL PRODUCTS</a></div>";
-                echo $editorialStr;
-            }
+            
         }
+
+        // if (!empty($output)) {
+            $shopUrl = get_permalink( wc_get_page_id( 'shop' ) );
+            echo $designerStr ? $designerStr : '';
+            echo "<div class='product-search searchedBlock'><h3>Products</h3><ul>";
+            echo $output;
+            echo "</ul></div>";
+            echo $editorialStr ? $editorialStr : '';
+        // }
     }
 
     die();
